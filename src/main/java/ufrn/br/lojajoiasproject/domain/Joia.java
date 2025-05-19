af9d5168330d09d520d317d09a78d1e3b86783bd
taniro <tanirocr@gmail.com>
2025-05-19 10:04:52 -0300

    update local

--- src/main/java/ufrn/br/lojajoiasproject/domain/Joia.java
+++ src/main/java/ufrn/br/lojajoiasproject/domain/Joia.java
@@ -25,4 +25,45 @@ public class Joia {
     Double valor;
     String tipo;
     String pedras;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getPedras() {
+        return pedras;
+    }
+
+    public void setPedras(String pedras) {
+        this.pedras = pedras;
+    }
 }
